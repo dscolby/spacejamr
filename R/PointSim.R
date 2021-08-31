cb615c421412157f50f069322aedc00a3700aed1
Darren Colby <dscolby17@gmail.com>
2021-08-31 18:06:57 -0400

    Added helper functions for PointSim class

--- R/PointSim.R
+++ R/PointSim.R
@@ -2,63 +2,6 @@
 # Date: 8/31/2021
 # Purpose: To simulate spatial point processes
 
-# Helper functions for the PointSim class ---------------------------------
-
-
-#' Simulate a Poisson point process
-#'
-#' @description helper function to simulate a Poisson point process
-#'
-#' @usage poisson_process(n, win)
-#'
-#' @details This function should not be called directly
-#'
-#' @param n the number of points to simulate
-#' @param win a window of class spacejamr to use as the spatial extent
-#'
-#' @return A ppp object from the 'spatstat.core' package that contains
-#' simulated points
-#'
-#' @author Darren Colby
-#' Email: dscolby17@gmail.com
-#' @noRd
-poisson_process <- function(n, win) {
-
-   # Homogeneous point process
-   point_process <- spatstat.core::rpoint(n = n, win = win)
-
-   return(point_process)
-
-}
-
-
-#' Simulate a 2D Halton Sequence
-#'
-#' @description helper function to simulate a 2D Halton Sequence
-#'
-#' @usage halton(n, win)
-#'
-#' @details This function should not be called directly
-#'
-#' @param n the number of points to simulate
-#' @param win a window of class spacejamr to use as the spatial extent
-#'
-#' @return A ppp object from the 'spatstat.geom' package that contains
-#' contains simulated points
-#'
-#' @author Darren Colby
-#' Email: dscolby17@gmail.com
-#' @noRd
-halton <- function(n, win) {
-
-   # Simulates the Halton sequence
-   halton_seq <- spatstat.geom::rQuasi(n, win, "Halton")
-
-   return(halton_seq)
-
-}
-
-
 # Constructor methods for the PointSim class ------------------------------
 
 #' Validate the input to an instance of a PointSim class
