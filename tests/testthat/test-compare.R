8ba0240b650c842d752be5fbfe750983f319459b
Darren Colby <dscolby17@gmail.com>
2021-09-06 13:23:49 -0400

    Suppressed warnings for unit tests

--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -1,9 +1,5 @@
 library(spacejamr)
 
-# Temporarily turn off warnings about centrality mesures of social networks
-old_warning <- getOption("warn")
-options(warn = -1)
-
 # Load data for the test
 data("RI")
 ri_points <- PointSim(3, RI)
@@ -13,12 +9,9 @@ apl <- NetSim(ri_points)
 test_that("we can compare summary statistics of two networks", {
 
     # Ensure the output is the correct class
-    expect_identical(class(compare_networks(pl, apl)), "data.frame")
+    expect_equal(class(suppressWarnings(compare_networks(pl, apl))), "data.frame")
 
     # Ensure the returned dataframe has the correct number of dimensions
-    expect_equal(dim(compare_networks(pl, apl)), c(2, 5))
+    expect_equal(dim(suppressWarnings(compare_networks(pl, apl))), c(2, 5))
 
 })
-
-# Turn warnings back on
-options(warn = old_warning)
