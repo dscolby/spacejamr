36c2aa9540dae090c58ab8351ffa05b8b499e74d
Darren Colby <dscolby17@gmail.com>
2022-01-27 17:58:03 -0500

    Skip on Appveyor

--- tests/testthat/test-spacejamr.R
+++ tests/testthat/test-spacejamr.R
@@ -2,6 +2,8 @@ library(spacejamr)
 
 skip_on_os("solaris")
 
+skip_on_appveyor()
+
 test_that("we can create spacejamr objects", {
 
     path <- system.file("shape", "ri.shp", package = "spacejamr")
