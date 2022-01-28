21ee8fe210432408cea3c942de885d9215afc341
Darren Colby <dscolby17@gmail.com>
2022-01-28 00:23:34 -0500

    Do not guess crs

--- tests/testthat/test-spacejamr.R
+++ tests/testthat/test-spacejamr.R
@@ -5,7 +5,7 @@ skip_on_os("solaris")
 test_that("we can create spacejamr objects", {
 
     path <- system.file("shape", "ri.shp", package = "spacejamr")
-    ri <- as.spacejamr(path)
+    ri <- as.spacejamr(path, guess_crs = FALSE)
 
     # Ensures the correct class
     expect_identical(class(ri), c("spacejamr", "owin"))
